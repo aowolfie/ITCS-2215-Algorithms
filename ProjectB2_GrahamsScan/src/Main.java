2cc2a99220bbe7f7bb79d6344d681b36f896835f
bluestar1996zz <bluestar1996zz@gmail.com>
2016-06-21 13:40:39 -0400

    Taking a moment to commit this because I want to try swing.

--- ProjectB2_GrahamsScan/src/Main.java
+++ ProjectB2_GrahamsScan/src/Main.java
@@ -1,4 +1,5 @@
-import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Stack;
@@ -9,31 +10,82 @@ import java.util.Stack;
  */
 public class Main {
 
+    //The file to read from
+    private final static String FILE_NAME = "grahamsInput.txt";
 
+    public static void main(String[] args){
+
+        String[] fileLines = null;
+
+        //Read in the file
+        try {
+            FileReader fReader = new FileReader(FILE_NAME);
+            BufferedReader bReader = new BufferedReader(fReader);
+            ArrayList<String> lines = new ArrayList<>();
+            String line = null;
+            while ((line = bReader.readLine()) != null){
+                lines.add(line);
+            }
+            fileLines = lines.toArray(new String[lines.size()]);
+        } catch (Exception e){
+
+        }
+
+        ArrayList<PointC> points = new ArrayList<PointC>();
+
+        for (String s: fileLines){
+            String[] sep = s.split(",");
+            points.add(new PointC(Integer.parseInt(sep[0]), Integer.parseInt(sep[1])));
+        }
+
+        ArrayList<PointC> pointsExtra = points;
+
+        Stack<PointC> s = scan(pointsExtra);
+
+        while(!s.isEmpty()){
+            System.out.println(s.pop());
+        }
+
+    }
 
     public static Stack<PointC> scan(ArrayList<PointC> list){
+        int index = 0;
+        for (int i=0; i < list.size(); i++){
+            if (list.get(i).getX() < list.get(index).getX()){
+                index = i;
+            } else if (list.get(i).getX() <= list.get(index).getX()){
+                if (list.get(i).getY() < list.get(index).getY()){
+                    index = i;
+                }
+            }
+        }
+
+        PointC bL = list.remove(index);
+
+        for (PointC pC: list){
+            pC.calculateSlope(bL);
+        }
+
+
         Stack<PointC> hull = new Stack<>();
         Collections.sort(list);
+
+        hull.add(bL);
+        hull.add(list.remove(0));
 
         while (list.size() > 0){
             PointC currentPoint = list.remove(0);
             while (true){
                 PointC previousPoint = hull.pop();
+                int rot = hull.peek().orientation(currentPoint, previousPoint);
+
+                if (rot <= 0) {
+                    hull.add(previousPoint);
+                    hull.add(currentPoint);
+                    break;
+                }
             }
         }
         return hull;
-    }
-
-    public static int orientation(PointC p1, PointC p2){
-        double val1 = p1.getY() * p2.getX();
-        double val2 = p1.getX() * p2.getY();
-
-        if (val1 < val2){
-            return -1; //Counter clockwise
-        } else if (val1 > val2){
-            return 1; //Clockwise
-        } else {
-            return 0; // Parallel
-        }
     }
 }
--- ProjectB2_GrahamsScan/src/PointC.java
+++ ProjectB2_GrahamsScan/src/PointC.java
@@ -7,12 +7,57 @@ import java.awt.*;
  */
 public class PointC extends Point implements Comparable<PointC> {
 
+    private double slope = 0;
+
+    public PointC(int x, int y){
+        super(x,y);
+    }
+
+    /**
+     * Using this point as the reference compares p1 and p2.
+     * p1 with either be inline with (parallel,
+     * to the left of (counterclockwise),
+     * or to the right of (clockwise) p2
+     *
+     * @param p1
+     * @param p2
+     * @return
+     */
+    public int orientation(PointC p1, PointC p2){
+        double xShift = this.getX() * -1;
+        double yShift = this.getY() * -1;
+        double val1 = (p1.getY() + yShift)* (p2.getX() + xShift);
+        double val2 = (p1.getX() + xShift) * (p2.getY() + yShift);
+
+        if (val1 > val2){
+            return -1; //Counter clockwise
+        } else if (val1 < val2){
+            return 1; //Clockwise
+        } else {
+            return 0; // Parallel
+        }
+    }
+
     public int compareTo(PointC point) {
-        if (this.getX() > point.getX() ){
+        if (this.getSlope() > point.getSlope() ){
             return 1;
-        } else if (this.getX() < point.getX()){
+        } else if (this.getSlope() < point.getSlope()){
             return -1;
         }
         return 0;
+    }
+
+    /**
+     * Calculates, stores and returns the slope
+     * @param point
+     * @return
+     */
+    public double calculateSlope(PointC point){
+        this.slope = ((point.getY() - this.getY())/(point.getX() - this.getX()));
+        return this.slope;
+    }
+
+    public double getSlope(){
+        return this.slope;
     }
 }
